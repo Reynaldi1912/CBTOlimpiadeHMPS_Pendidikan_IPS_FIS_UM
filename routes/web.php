74d5cf5f3a039c05178ee5c581615c2b41e486f3
Zidane <zidanesanjaya81@Gmail.com>
2023-03-31 20:45:35 +0700

    z

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// page section
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/login_admin', function () {
+    return view('/auth/admin/adminLogin');
 });
+
+Route::post('/adminDashboard', function () {
+    return view('admin/admin');
+});
+// end page section
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
