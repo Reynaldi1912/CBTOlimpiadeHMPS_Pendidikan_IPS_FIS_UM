61c0bbc49b0dbf409903bd7ab191802654ecb172
Zidane <zidanesanjaya81@Gmail.com>
2023-04-02 22:38:34 +0700

    z

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -157,37 +157,4 @@
                 <!-- END Page Content -->
         </div>
         <!-- END Page Container -->
-
-        <!--
-            Codebase JS Core
-
-            Vital libraries and plugins used in all pages. You can choose to not include this file if you would like
-            to handle those dependencies through webpack. Please check out /codebase/_es6/main/bootstrap.js for more info.
-
-            If you like, you could also include them separately directly from the /codebase/js/core folder in the following
-            order. That can come in handy if you would like to include a few of them (eg jQuery) from a CDN.
-
-            /codebase/js/core/jquery.min.js
-            /codebase/js/core/bootstrap.bundle.min.js
-            /codebase/js/core/simplebar.min.js
-            /codebase/js/core/jquery-scrollLock.min.js
-            /codebase/js/core/jquery.appear.min.js
-            /codebase/js/core/jquery.countTo.min.js
-            /codebase/js/core/js.cookie.min.js
-        -->
-        <script src="/codebase/js/codebase.core.min.js"></script>
-
-        <!--
-            Codebase JS
-
-            Custom functionality including Blocks/Layout API as well as other vital and optional helpers
-            webpack is putting everything together at /codebase/_es6/main/app.js
-        -->
-        <script src="/codebase/js/codebase.app.min.js"></script>
-
-        <!-- Page JS Plugins -->
-        <script src="/codebase/js/plugins/chartjs/Chart.bundle.min.js"></script>
-
-        <!-- Page JS Code -->
-        <script src="/codebase/js/pages/be_pages_dashboard.min.js"></script>
 @endsection
--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -36,51 +36,7 @@
         <!-- END Stylesheets -->
     </head>
     <body>
-
         <!-- Page Container -->
-        <!--
-            Available classes for #page-container:
-
-        GENERIC
-
-            'enable-cookies'                            Remembers active color theme between pages (when set through color theme helper Template._uiHandleTheme())
-
-        SIDEBAR & SIDE OVERLAY
-
-            'sidebar-r'                                 Right Sidebar and left Side Overlay (default is left Sidebar and right Side Overlay)
-            'sidebar-mini'                              Mini hoverable Sidebar (screen width > 991px)
-            'sidebar-o'                                 Visible Sidebar by default (screen width > 991px)
-            'sidebar-o-xs'                              Visible Sidebar by default (screen width < 992px)
-            'sidebar-inverse'                           Dark themed sidebar
-
-            'side-overlay-hover'                        Hoverable Side Overlay (screen width > 991px)
-            'side-overlay-o'                            Visible Side Overlay by default
-
-            'enable-page-overlay'                       Enables a visible clickable Page Overlay (closes Side Overlay on click) when Side Overlay opens
-
-            'side-scroll'                               Enables custom scrolling on Sidebar and Side Overlay instead of native scrolling (screen width > 991px)
-
-        HEADER
-
-            ''                                          Static Header if no class is added
-            'page-header-fixed'                         Fixed Header
-
-        HEADER STYLE
-
-            ''                                          Classic Header style if no class is added
-            'page-header-modern'                        Modern Header style
-            'page-header-inverse'                       Dark themed Header (works only with classic Header style)
-            'page-header-glass'                         Light themed Header with transparency by default
-                                                        (absolute position, perfect for light images underneath - solid light background on scroll if the Header is also set as fixed)
-            'page-header-glass page-header-inverse'     Dark themed Header with transparency by default
-                                                        (absolute position, perfect for dark images underneath - solid dark background on scroll if the Header is also set as fixed)
-
-        MAIN CONTENT LAYOUT
-
-            ''                                          Full width Main Content if no class is added
-            'main-content-boxed'                        Full width Main Content with a specific maximum width (screen width > 1200px)
-            'main-content-narrow'                       Full width Main Content with a percentage width (screen width > 1200px)
-        -->
         <div id="page-container" class="sidebar-o sidebar-inverse enable-page-overlay side-scroll page-header-fixed page-header-modern main-content-boxed">
             <!-- Side Overlay-->
             <aside id="side-overlay">
