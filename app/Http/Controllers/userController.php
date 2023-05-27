df1b6235d5ded4f5530ad40835a158732347f6da
Reynaldi1912 <nexrey19@gmail.com>
2023-05-27 18:04:45 +0700

    update name controller

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
-class userController extends Controller
+class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
