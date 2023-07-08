3ed12aadb30b9791fd89fb1e63b42e238bfb5fe6
Zidane <zidanesanjaya81@Gmail.com>
2023-07-08 23:50:23 +0700

    z

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,6 +3,9 @@
 @section('content')
                 <!-- Page Content -->
                 <div class="content">
+                    <div class="text-center" data-toggle="appear">
+                        <h1 class="font-w700 text-bg-dark mb-100">Halaman Admin Olimpiade Pendidikan IPS</h1>
+                    </div>
                     <div class="row invisible" data-toggle="appear">
                         <!-- Row #1 -->
                         <div class="col-6 col-xl-6">
@@ -29,9 +32,60 @@
                         </div>
                         <!-- END Row #1 -->
                     </div>
-                    
-                    
-
+                    <div class="row invisible" data-toggle="appear">
+                        <div class="col-4">
+                            <a class="block block-link-shadow text-center" href="{{route('ujianAdmin.index')}}">
+                                <div class="block-content">
+                                    <p class="mt-5">
+                                        <i class="si si-notebook fa-3x"></i>
+                                    </p>
+                                    <p class="font-w600">Kelola Ujian</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-4">
+                            <a class="block block-link-shadow text-center" href="{{route('question-admin.index')}}">
+                                <div class="block-content">
+                                    <p class="mt-5">
+                                        <i class="si si-lock fa-3x"></i>
+                                    </p>
+                                    <p class="font-w600">Soal</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-4">
+                            <a class="block block-link-shadow text-center" href="{{route('hasil-ujian.index')}}">
+                                <div class="block-content">
+                                    <p class="mt-5">
+                                        <i class="si si-badge fa-3x"></i>
+                                    </p>
+                                    <p class="font-w600">Hasil Ujian</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="row invisible" data-toggle="appear">
+                        <div class="col-6">
+                            <a class="block block-link-shadow text-center" href="{{route('user.index')}}">
+                                <div class="block-content">
+                                    <p class="mt-5">
+                                        <i class="si si-user fa-3x"></i>
+                                    </p>
+                                    <p class="font-w600">Kelola Admin</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-6">
+                            <a class="block block-link-shadow text-center" href="{{route('user.create')}}">
+                                <div class="block-content">
+                                    <p class="mt-5">
+                                        <i class="fa fa-users fa-3x"></i>
+                                    </p>
+                                    <p class="font-w600">Kelola Peserta</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
                 </div>
                 <!-- END Page Content -->
         </div>
